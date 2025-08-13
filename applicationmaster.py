d73e43421aa7a00c86159a072a9653f94c7fab74
Gitansh Syal <g24ai1079@iitj.ac.in>
2025-08-13 11:53:05 +0530

    Aggregations

--- applicationmaster.py
+++ applicationmaster.py
@@ -1,5 +1,5 @@
 import sys
-from lib import DataReader, utils
+from lib import DataReader, utils,DataManipulation
 from pyspark.sql.functions import *
 if __name__ == '__main__':
   if len(sys.argv) < 2:
@@ -12,3 +12,11 @@ print("Created Spark Session")
 order_df=DataReader.get_order_df(spark,job_run_env)
 customer_df=DataReader.get_customer_df(spark,job_run_env)
 product_df=DataReader.get_product_df(spark,job_run_env)
+filter_df=DataManipulation.filter_order_status(order_df,'DELIVERED')
+total_sales_df=DataManipulation.order_status_sales(order_df,'order_status')
+join_df=DataManipulation.join_df(order_df,customer_df,'customer_id')
+filter_join_df=DataManipulation.filter_count_customers(join_df,1)
+avg_total_price_df=DataManipulation.avg_order_price(join_df)
+join_df1=DataManipulation.join_df(join_df,product_df,'product_id')
+top_products_df=DataManipulation.top_expensive_per_department(join_df1,'category')
+print(top_products_df.select('order_id','product_id','category','total_price','rank').show())
--- lib/DataReader.py
+++ lib/DataReader.py
@@ -2,23 +2,25 @@ from lib.ConfigReader import get_app_config
 from lib.utils import get_spark_session
 
 def getOrderSchema():
-    return '''order_id int,customer_id int,product_id int,order_date date,order_status string,total_price double
+    return '''order_id int,customer_id int,product_id int,order_date date,order_status string,quantity int,total_price double
 '''
 def get_order_df(spark,env):
     conf=get_app_config(env)
     file_path=conf['order.file.path']
     return spark.read.format('csv') \
           .schema(getOrderSchema()) \
+          .option('header',True) \
           .load(file_path)
 
 def get_customer_schema():
-    return '''customer_id int,customer_name string,city string,state string
+    return '''customer_id int,customer_name string,email string,city string,state string,signup_date date
 '''
 def get_customer_df(spark,env):
    conf=get_app_config(env)
    file_path=conf['customer.file.path']
    return spark.read.format('csv') \
          .schema(get_customer_schema()) \
+          .option('header',True) \
          .load(file_path)
 
 def get_product_schema():
@@ -29,4 +31,5 @@ def get_product_df(spark,env):
     file_path=conf['product.file.path']
     return spark.read.format('csv') \
          .schema(get_product_schema()) \
+          .option('header',True) \
          .load(file_path)
